bc0b2f54bbfe9516818567d268292479a507e7a6
Colin Williams <lackita@gmail.com>
2013-07-25 17:43:27 -0400

    org customizations

--- init.el
+++ init.el
@@ -39,23 +39,6 @@
  '(kill-whole-line t)
  '(make-backup-files nil)
  '(menu-bar-mode nil)
- '(org-agenda-custom-commands (quote (("n" "Agenda and all TODO's" ((agenda "") (alltodo))) ("w" "Work" tags "+work-waiting-maybe-SCHEDULED>\"<now>\"/TODO") ("h" "Home" tags "-waiting-maybe-work-SCHEDULED>\"<now>\"/TODO") ("c" "Couch" tags "+internet-adrianna-desktop-waiting-maybe-work-SCHEDULED>\"<now>\"/TODO"))))
- '(org-agenda-include-diary t)
- '(org-agenda-ndays 1)
- '(org-agenda-skip-deadline-prewarning-if-scheduled t)
- '(org-agenda-skip-scheduled-if-done t)
- '(org-agenda-window-setup (quote current-window))
- '(org-archive-location "~/Dropbox/org/archive/%s_archive::")
- '(org-deadline-warning-days 4)
- '(org-directory "~/Dropbox/org")
- '(org-file-apps (quote ((auto-mode . emacs) ("\\.mm\\'" . default) ("\\.x?html?\\'" . default) ("\\.pdf\\'" . default) ("\\.ppt\\'" . default))))
- '(org-habit-show-habits-only-for-today nil)
- '(org-link-abbrev-alist (quote (("hydra" . "https://intranet.athenahealth.com/hydra/taskview.esp?ID=") ("wiki" . "https://intranet.athenahealth.com/wiki/node.esp?ID=") ("hermes" . "https://intranet.athenahealth.com/hermes/queue/view.esp?JOBID="))))
- '(org-log-done (quote time))
- '(org-log-into-drawer t)
- '(org-mobile-inbox-for-pull "~/Dropbox/org/from-mobile.org")
- '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-rmail org-vm org-wl org-w3m org-checklist org-learn org-registry org2rem)))
- '(org-scheduled-past-days 1)
  '(paren-mode (quote paren) nil (paren))
  '(safe-local-variable-values (quote ((encoding . utf-8))))
  '(scroll-bar-mode nil)
@@ -75,8 +58,6 @@
  ;; If there is more than one, they won't work right.
  )
 
-(require 'find-lisp)
-(setq org-agenda-files (find-lisp-find-files "~/Dropbox/org" "\.org$"))
 
 (defalias 'perl-mode 'cperl-mode)
 (push '("\\.diffrule\\'" . cperl-mode) auto-mode-alist)
@@ -158,14 +139,7 @@
 (require 'undo-tree)
 (global-undo-tree-mode)
 
-(require 'org)
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
-(setq org-mobile-directory "~/Dropbox/MobileOrg")
-;(org-agenda-to-appt)
-
 (global-hl-line-mode t)
 
 ;; (require 'editortools)
+(require 'org-customizations)
