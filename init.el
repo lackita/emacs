82f0713d69bd615cecdc36d678c50bfddd5cae53
Colin Williams <cwilliams@cwilliams-mac15.local>
2013-08-08 10:52:55 -0400

    pre-pull value

--- init.el
+++ init.el
@@ -35,11 +35,6 @@
  ;; If there is more than one, they won't work right.
  )
 
-(set-default buffer-file-coding-system 'utf-8-unix)
-(set-default-coding-systems 'utf-8-unix)
-(prefer-coding-system 'utf-8-unix)
-(set-default default-buffer-file-coding-system 'utf-8-unix)
-
 (put 'narrow-to-region 'disabled nil)
 
 (ido-mode)
